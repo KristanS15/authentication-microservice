dc068262d32589e174ae8bf7274bd3e6de3ba006
Kristan Sirett <kristansirett15@gmail.com>
2019-08-05 16:09:08 +0100

    remove knexfile environment variable

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,17 @@
 // Update with your config settings.
 
+require('dotenv').config()
+
 module.exports = {
-	[process.env.ENV || "production"]: {
-		client: process.env.DB_CLIENT,
-		connection: {
-			host: process.env.DB_HOST,
-			user: process.env.DB_USER,
-			password: process.env.DB_PASS,
-			database: process.env.DB_NAME,
-			charset: process.env.DB_CHARSET || 'utf8'
-		},
-		migrations: {
-			tableName: 'knex_migrations'
-		}
+	client: process.env.DB_CLIENT,
+	connection: {
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB_NAME,
+		charset: process.env.DB_CHARSET || 'utf8'
+	},
+	migrations: {
+		tableName: 'knex_migrations'
 	}
 };
